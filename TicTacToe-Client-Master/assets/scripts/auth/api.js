72de1df0ad092e9a50487d720b5e94f6c04c2b13
bgordon83 <ben.gordon1983@gmail.com>
2019-10-14 08:25:40 -0400

    still working
    I still need a little more time, been having some difficulty with merging..

---
+++ TicTacToe-Client-Master/assets/scripts/auth/api.js
@@ -0,0 +1,53 @@
+'use strict'
+
+coconst config = require('../config.js')
+const store = require('../store.js')
+
+const signUp = function (formData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data: formData
+  })
+}
+
+const signIn = function (formData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-in',
+    data: formData,
+    success: function () {
+
+    }
+  })
+}
+
+const changePassword = function (formData) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    success: function () {
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
---
+++ TicTacToe-Client-Master/curl-scripts/auth/Game.sh/API.js
@@ -0,0 +1,5 @@
+
+const config = require('../config.js')
+const store = require('../store.js')
+
+const app = require('./app-data.js');
